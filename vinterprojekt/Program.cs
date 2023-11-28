408e47173a7a5181e409d3cec6b362ff235da84d
Mikael Edvardsson <Mikael.Edvardsson@elev.ga.ntig.se>
2023-11-28 11:02:35 +0100

    experiment

--- vinterprojekt/Program.cs
+++ vinterprojekt/Program.cs
@@ -4,14 +4,31 @@ using Raylib_cs;
 Raylib.InitWindow(1800, 900, "vinterprojekt");
 Raylib.SetTargetFPS(60);
 
+Rectangle p = new Rectangle(50,50,50,50);
+Rectangle e = new Rectangle(850,50,50,50);
+
+
+
+
 
 while (!Raylib.WindowShouldClose())
 {
+
+
+
   Raylib.BeginDrawing();
+
+
   
   Raylib.ClearBackground(Color.WHITE);
+
+  //List<Rectangle> = new List<Rectangle>();
+
+
   
-  Raylib.DrawRectangle(0,850,1800,50, Color.MAGENTA);
+  Raylib.DrawRectangleRec(p, Color.SKYBLUE);
+  Raylib.DrawRectangleRec(e, Color.RED);
+  bool Overlapping = Raylib.CheckCollisionRecs(pRect, eRect);
   
   Raylib.EndDrawing();
 }
