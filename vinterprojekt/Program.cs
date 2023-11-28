8e3844b550086b9cc5c927f7ed8ac262b74393dd
Mikael Edvardsson <Mikael.Edvardsson@elev.ga.ntig.se>
2023-11-28 10:33:10 +0100

    började göra basics och lägga golv

--- vinterprojekt/Program.cs
+++ vinterprojekt/Program.cs
@@ -1,3 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Raylib_cs;
 
+Raylib.InitWindow(1800, 900, "vinterprojekt");
+Raylib.SetTargetFPS(60);
+
+
+while (!Raylib.WindowShouldClose())
+{
+  Raylib.BeginDrawing();
+  
+  Raylib.ClearBackground(Color.WHITE);
+  
+  Raylib.DrawRectangle(0,850,1800,50, Color.MAGENTA);
+  
+  Raylib.EndDrawing();
+}
