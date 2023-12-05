d0adc546d5f8a0007f80ad334b2047f35dbd9b67
Mikael Edvardsson <Mikael.Edvardsson@elev.ga.ntig.se>
2023-12-05 10:34:34 +0100

    fixat hitbox halvt

--- vinterprojekt/Program.cs
+++ vinterprojekt/Program.cs
@@ -8,7 +8,11 @@ Raylib.SetTargetFPS(60);
 
 //List<Rectangle> = new List<Rectangle>();
 
-
+Rectangle playerRect = new Rectangle(50, 50, 100, 100);
+Rectangle enemyRect = new Rectangle(1000, 100, 100, 100);
+playerRect.X = 0;
+playerRect.Y = 0;
+string scene = "start";
 
 while (!Raylib.WindowShouldClose())
 {
@@ -16,39 +20,48 @@ while (!Raylib.WindowShouldClose())
 
 
 
-
   Raylib.BeginDrawing();
 
-
-  
   Raylib.ClearBackground(Color.WHITE);
 
-  
-Rectangle playerRect = new Rectangle(50,50,100,100);
-Rectangle enemyRect = new Rectangle(1000,100,100,100);
-Rectangle overlap = Raylib.GetCollisionRec(playerRect, enemyRect);
+  Rectangle overlap = Raylib.GetCollisionRec(playerRect, enemyRect);
 
-Raylib.DrawRectangleRec(playerRect, Color.RED);
-Raylib.DrawRectangleRec(enemyRect, Color.BLUE);
-Raylib.DrawRectangleRec(overlap, Color.ORANGE);
+  Raylib.DrawRectangleRec(playerRect, Color.RED);
+  Raylib.DrawRectangleRec(enemyRect, Color.BLUE);
+  Raylib.DrawRectangleRec(overlap, Color.ORANGE);
 
 
-string scene = "start";
+  if (scene == "start")
+  {
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_D))
+    {
+      playerRect.X += 5;
+    }
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_A))
+    {
+      playerRect.X -= 5;
+    }
 
-if (scene == "start")
-{
-   if (Raylib.IsKeyDown(KeyboardKey.KEY_D))
-{
-playerRect.X +=1;
-}
-   if (Raylib.IsKeyDown(KeyboardKey.KEY_A))
-{
-playerRect.X -=1;
-}
+      if (Raylib.IsKeyDown(KeyboardKey.KEY_W))
+    {
+      playerRect.Y -= 5;
+    }
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_S))
+    {
+      playerRect.Y +=5;
+    }
+     
+    if (playerRect.X < 0 || playerRect.X > 1699) {
+    playerRect.X *= -1;
+  }
 
-}
+  if (playerRect.Y < 0  || playerRect.Y > 799) {
+    playerRect.Y *= -1;
+  }
 
-  
-Raylib.EndDrawing();
+  }
+
+
+  Raylib.EndDrawing();
 
 }
