cd46073d6d68cf9defe0c6a13760babf4e45f425
Mikael Edvardsson <Mikael.Edvardsson@elev.ga.ntig.se>
2023-12-12 11:05:13 +0100

    fixat walls

--- vinterprojekt/Program.cs
+++ vinterprojekt/Program.cs
@@ -6,13 +6,13 @@ Raylib.SetTargetFPS(60);
 
 
 
-List<Rectangle> wall = new();
+List<Rectangle> walls = new();
 
 
-wall.Add(new Rectangle(300, 100, 60, 20));
-wall.Add(new Rectangle(320, 0, 16, 20));
-wall.Add(new Rectangle(150, 0, 32, 128));
-wall.Add(new Rectangle(123, 0, 1800, 20));
+walls.Add(new Rectangle(300, 100, 60, 20));
+walls.Add(new Rectangle(320, 0, 16, 20));
+walls.Add(new Rectangle(150, 0, 32, 128));
+walls.Add(new Rectangle(123, 0, 1800, 20));
 
 Rectangle playerRect = new Rectangle(50, 50, 100, 100);
 Rectangle enemyRect = new Rectangle(1000, 100, 100, 100);
@@ -24,9 +24,6 @@ scene = "start";
 while (!Raylib.WindowShouldClose())
 {
 
-
-
-
   Raylib.BeginDrawing();
 
   Raylib.ClearBackground(Color.WHITE);
@@ -37,19 +34,28 @@ while (!Raylib.WindowShouldClose())
   Raylib.DrawRectangleRec(enemyRect, Color.BLUE);
   Raylib.DrawRectangleRec(overlap, Color.ORANGE);
 
-  if (scene == "start"){
+  foreach (Rectangle wall in walls)
+  {
+    Raylib.DrawRectangleRec(wall, Color.DARKBLUE);
+  }
 
-    Console.WriteLine("welcome to the game");
+  if (scene == "start")
+  {
+
+    Raylib.ClearBackground(Color.BLACK);
+    Raylib.DrawRectangle(300, 300, 40, 40, Color.RED);
+    Raylib.DrawText("lets go", 300, 300, 40, Color.YELLOW);
+
 
   }
 
-  
+
 
 
   if (scene == "game")
   {
 
-    enemyRect.Y +=1;
+    enemyRect.Y += 1;
 
     if (Raylib.IsKeyDown(KeyboardKey.KEY_D))
     {
@@ -60,44 +66,46 @@ while (!Raylib.WindowShouldClose())
       playerRect.X -= 5;
     }
 
-      if (Raylib.IsKeyDown(KeyboardKey.KEY_W))
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_W))
     {
       playerRect.Y -= 5;
     }
     if (Raylib.IsKeyDown(KeyboardKey.KEY_S))
     {
-      playerRect.Y +=5;
+      playerRect.Y += 5;
     }
-     
- if (playerRect.X < 0 || playerRect.X > 1699) {
- playerRect.X = -1;
-  }
 
-if (playerRect.Y < 0  || playerRect.Y > 799) {
-playerRect.Y = -1;
-
-  }
-
-
-
-/*bool isInAWall = CheckIfWall(playerRect, wall);
-
-  if (isInAWall == true)
+    if (playerRect.X < 0 || playerRect.X > 1699)
     {
-    
+      playerRect.X = -1;
     }
-*/
- /*foreach (Rectangle wall in walls)
-   {
-    if (Raylib.CheckCollisionRecs(playerRect, wall))
+
+    if (playerRect.Y < 0 || playerRect.Y > 799)
     {
-  playerRect.X *= -1;
-  playerRect.Y *= -1;
+      playerRect.Y = -1;
 
     }
-  }*/
 
 
+
+    /*bool isInAWall = CheckIfWall(playerRect, wall);
+
+      if (isInAWall == true)
+        {
+
+        }
+    */
+    /*foreach (Rectangle wall in walls)
+      {
+       if (Raylib.CheckCollisionRecs(playerRect, wall))
+       {
+     playerRect.X *= -1;
+     playerRect.Y *= -1;
+
+       }
+     }*/
+
+
+  }
   Raylib.EndDrawing();
-  }
 }
