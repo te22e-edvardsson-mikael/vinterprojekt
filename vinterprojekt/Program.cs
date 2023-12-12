4c52e8ed5055d305112e642598dedfeee1e7bea8
Mikael Edvardsson <Mikael.Edvardsson@elev.ga.ntig.se>
2023-12-12 10:50:38 +0100

    ..

--- vinterprojekt/Program.cs
+++ vinterprojekt/Program.cs
@@ -6,19 +6,20 @@ Raylib.SetTargetFPS(60);
 
 
 
-List<Rectangle> walls = new();
+List<Rectangle> wall = new();
 
 
-Rectangle wall1 = new Rectangle(123, 0, 1800, 20);
-Rectangle wall2 = new Rectangle(150, 0, 32, 128);
-Rectangle wall3 = new Rectangle(320, 0, 16, 20);
-Rectangle wall4 = new Rectangle(300, 100, 60, 20);
+wall.Add(new Rectangle(300, 100, 60, 20));
+wall.Add(new Rectangle(320, 0, 16, 20));
+wall.Add(new Rectangle(150, 0, 32, 128));
+wall.Add(new Rectangle(123, 0, 1800, 20));
 
 Rectangle playerRect = new Rectangle(50, 50, 100, 100);
 Rectangle enemyRect = new Rectangle(1000, 100, 100, 100);
-playerRect.X = 0;
-playerRect.Y = 0;
-string scene = "start";
+
+string scene;
+scene = "game";
+scene = "start";
 
 while (!Raylib.WindowShouldClose())
 {
@@ -36,9 +37,20 @@ while (!Raylib.WindowShouldClose())
   Raylib.DrawRectangleRec(enemyRect, Color.BLUE);
   Raylib.DrawRectangleRec(overlap, Color.ORANGE);
 
+  if (scene == "start"){
 
-  if (scene == "start")
+    Console.WriteLine("welcome to the game");
+
+  }
+
+  
+
+
+  if (scene == "game")
   {
+
+    enemyRect.Y +=1;
+
     if (Raylib.IsKeyDown(KeyboardKey.KEY_D))
     {
       playerRect.X += 5;
@@ -58,23 +70,34 @@ while (!Raylib.WindowShouldClose())
     }
      
  if (playerRect.X < 0 || playerRect.X > 1699) {
- playerRect.X *= -1;
+ playerRect.X = -1;
   }
 
 if (playerRect.Y < 0  || playerRect.Y > 799) {
-playerRect.Y *= -1;
+playerRect.Y = -1;
+
   }
 
 
- foreach (Rectangle wall in walls)
+
+/*bool isInAWall = CheckIfWall(playerRect, wall);
+
+  if (isInAWall == true)
+    {
+    
+    }
+*/
+ /*foreach (Rectangle wall in walls)
    {
     if (Raylib.CheckCollisionRecs(playerRect, wall))
     {
-  playerRect.X = 0;
-  playerRect.Y = 0;
+  playerRect.X *= -1;
+  playerRect.Y *= -1;
 
     }
-  }
+  }*/
+
+
   Raylib.EndDrawing();
   }
 }
